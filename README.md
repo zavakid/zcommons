a00f97a3091850245c2819db847fdff9d70df905
zava <zava.kid@gmail.com>
2013-05-27 22:42:55 +0800

    add webdriver

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-my commons code
+## zcommons
+
+### commons.test
+1. dependency casperjs for function test, must install casperjs before: http://casperjs.org/installation.html
--- commons.jetty/src/test/java/com/zavakid/commons/jetty/JettyServerTest.java
+++ commons.jetty/src/test/java/com/zavakid/commons/jetty/JettyServerTest.java
@@ -13,7 +13,20 @@
  */
 package com.zavakid.commons.jetty;
 
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.zavakid.commons.jetty.servlet.TestServlet;
 
 /**
  * @author zavakid 2013-5-25 上午9:29:40
@@ -21,9 +34,12 @@ import org.junit.Test;
  */
 public class JettyServerTest {
 
+    private static ChromeDriverService service;
+    private WebDriver                  driver;
+
     @Test
     public void testJettyServer() throws InterruptedException {
-        JettyServer httpServer = new JettyServer();
+        final JettyServer httpServer = new JettyServer();
         httpServer.setContextPath("/");
         httpServer.setMaxThreads(10);
         httpServer.setParentLoaderPriority(true);
@@ -31,8 +47,49 @@ public class JettyServerTest {
         httpServer.setResourceBase(JettyServerTest.class.getResource("/test-web").toString());
         httpServer.setThreadName("jetty-test-thread");
         httpServer.start();
-        httpServer.join();
-        httpServer.stop();
+        try {
+            testPage();
+        } finally {
+            httpServer.stop();
+        }
+    }
 
+    private void testPage() {
+        // driver = new RemoteWebDriver(service.getUrl(),
+        // DesiredCapabilities.chrome());
+        driver = new FirefoxDriver();
+        driver.get("http://127.0.0.1:8080/test");
+        String content = driver.getPageSource();
+        System.out.println("Page content is: " + content);
+        assertEquals(TestServlet.OUTPUT_TEXT, content);
+        driver.quit();
+    }
+
+    @BeforeClass
+    public static void createAndStartService() throws IOException {
+        // service = new ChromeDriverService.Builder().usingDriverExecutable(new
+        // File(getChromeDriverPath()))
+        // .usingAnyFreePort()
+        // .build();
+        // service.start();
+    }
+
+    @AfterClass
+    public static void createAndStopService() {
+        // service.stop();
+    }
+
+    public static String getChromeDriverPath() {
+        URL url = JettyServerTest.class.getResource("/");
+        File file = new File(url.toString());
+        File rootProjectPath = file.getParentFile().getParentFile().getParentFile();
+        String chromeDriverPath = rootProjectPath.toString() + File.separator + "lib" + File.separator + "chromedriver";
+        if (chromeDriverPath.startsWith("file:")) {
+            chromeDriverPath = chromeDriverPath.substring("file:".length(), chromeDriverPath.length());
+        }
+        if (!chromeDriverPath.startsWith("/")) {
+            chromeDriverPath = "/" + chromeDriverPath;
+        }
+        return chromeDriverPath;
     }
 }
