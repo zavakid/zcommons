872a7e59d969cdd948c3d51f80b72103d92a13c0
zava <zava.kid@gmail.com>
2013-08-11 15:12:04 +0800

    rename casperjs executor and exception

--- commons.test/src/main/java/com/zavakid/commons/test/casperjs/CasperJsExecutor.java
+++ commons.test/src/main/java/com/zavakid/commons/test/casperjs/CasperJsExecutor.java
@@ -11,7 +11,7 @@
    See the License for the specific language governing permissions and
    limitations under the License.
  */
-package com.zavakid.commons.test.web;
+package com.zavakid.commons.test.casperjs;
 
 import org.apache.commons.exec.CommandLine;
 import org.apache.commons.exec.DefaultExecutor;
